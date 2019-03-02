e038316a9f5351c1c7876bae75acfd846068f948
jaf9897 <jaf9897@rit.edu>
2019-03-01 23:29:42 -0500

    Added small brain check

--- peopleBot.py
+++ peopleBot.py
@@ -6,21 +6,38 @@ TOKEN = 'NTUwNTAyNjgwNTM2MDIzMDQx.D1jiQQ.Y9f_MmsbsZcP8cdSVEaw18CFPyo'
 
 client = discord.Client()
 
+# Main functionality, sends TPDNE image
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
+    # If person is sent
     if message.content.startswith('!person'):
         msg = 'Generating image...'.format(message)
         await client.send_message(message.channel, msg)
         imgurl ="https://thispersondoesnotexist.com/image"
-        req.urlretrieve(imgurl, "image.jpg")
+        req.urlretrieve(imgurl, "image.jpg")  # Download TPDNE image
         with open('image.jpg', 'rb') as f:
             msg = 'This person does not exist: '.format(message)
             await client.send_message(message.channel, msg)
-            await client.send_file(message.channel, f)
+            await client.send_file(message.channel, f) 
+
+# Respond to small brain reaction.
+@client.event
+async def on_reaction_add(reaction, user):
+    channel = reaction.message.channel
+    emoji = reaction.emoji
+    if isinstance(emoji, discord.Emoji):
+        name = emoji.name
+    elif isinstance(emoji, str):
+        name = emoji
+    else:
+        raise ValueError("Unknown emoji of type:", type(emoji))
+
+    if name == "20":
+        await client.send_message(channel, "There is nothing more small brained than small braining a machine, you coward.")
 
 @client.event
 async def on_ready():
