bb177c2c32bb47ebaf00cdfc575c60f78a32db26
jaf9897 <jaf9897@rit.edu>
2019-07-11 02:49:49 -0400

    Added custom mentions and creation of custom mentions

--- peopleBot.py
+++ peopleBot.py
@@ -8,7 +8,7 @@ TOKEN = 'NTUwNTAyNjgwNTM2MDIzMDQx.D1jiQQ.Y9f_MmsbsZcP8cdSVEaw18CFPyo'
 blakes = open("blake.txt", "r")
 proverbs = blakes.read().split('^')
 blakes.close()
-
+mentions = dict()
 client = discord.Client()
 
 # Awaits commands and executes accordingly
@@ -36,7 +36,7 @@ async def on_message(message):
         """
 
         # Old code from when it was presumed dead.
-        #msg = "Farewell to thee, farewell to thee \nThou charming one who dwells in shaded bowers \nOne fond embrace ere I depart \nUntil we meet again." 
+        """msg = "Farewell to thee, farewell to thee \nThou charming one who dwells in shaded bowers \nOne fond embrace ere I depart \nUntil we meet again." """
         
         msg = random.choice(proverbs)
         await client.send_message(message.channel, msg)
@@ -45,20 +45,70 @@ async def on_message(message):
     
     # Waits for '!friend' to resolve conflicts
     elif message.content.startswith('!friend'):
-        youtube_url = 'https://www.youtube.com/watch?v=htcvoz8x_qY'
-        channel = message.author.voice.voice_channel
-        voice = await client.join_voice_channel(channel)
-        player = voice.create_ffmpeg_player('friends.mp3', after=lambda: print('Song done'))
-        player.start()
-        while not player.is_done():
-            await asyncio.sleep(1) # Will leave when the entire song is finished 
-        player.stop()
-        await voice.disconnect()
+        try:
+            youtube_url = 'https://www.youtube.com/watch?v=htcvoz8x_qY'
+            channel = message.author.voice.voice_channel
+            voice = await client.join_voice_channel(channel)
+            player = voice.create_ffmpeg_player('friends.mp3', after=lambda: print('Song done'))
+            player.start()
+            while not player.is_done():
+                await asyncio.sleep(1) # Will leave when the entire song is finished 
+            player.stop()
+            await voice.disconnect()
+        except:
+            await client.send_message(message.channel, "Must be in a voice channel to resolve conflicts.")
 
     # Allows for early conflict resolution
     elif message.content.startswith('!resolved'):
         for x in client.voice_clients:
             return await x.disconnect()
+
+    elif message.content.startswith('@'):
+        content = message.content[1:]
+        content = content.split(' ', 1)
+        if content[0] in mentions.keys():
+            people = mentions.get(content[0])
+            print(people)
+            await client.send_message(message.channel, content[0] + " invoked.")
+            final_mention = ""
+            for person in people:
+                person = person.split("#")
+                print(person)
+                user = discord.utils.get(message.server.members, name = person[0], discriminator = person[1])
+                final_mention += user.mention + " "
+            try:
+                final_mention += "\n" + content[1]
+            except:
+                pass
+            await client.send_message(message.channel, final_mention)
+
+    elif message.content.startswith('!add'):
+        await client.send_message(message.channel, "Creating new mention...")
+        content = message.content[5:]
+        content = content.split(" ", 1)
+        mention = content[0]
+        names = content[1].split("/")
+        final_string = mention + ";"
+        valid_names = True
+        print(mention)
+        print(names)
+        for x in names:
+            print(x)
+            if not x[-5] == "#":
+                await client.send_message(message.channel, "Invalid username: " + x)
+                valid_names = False
+            else:
+                final_string += x + ","
+        if valid_names:
+            final_string = final_string.rstrip(",")
+            fp = open("mentions.txt", "a")
+            fp.write(final_string)
+            fp.close
+            mentions[mention] = names
+            await client.send_message(message.channel, "Successfully created @" + mention)
+        else:
+            await client.send_message(message.channel, "Unable to create new mention")
+
 
 # Respond to small brain reaction.
 @client.event
@@ -82,9 +132,12 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    
-
-
+    fp = open("mentions.txt", "r")
+    for x in fp:
+        x = x.rstrip("\n")
+        x = x.split(';')
+        mentions[x[0]] = x[1].split(",")
+    fp.close()
 
 
 
